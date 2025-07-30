7e4ac990e42cd948221e7321f6fa00213651d094
BobbyMarley001 <nexusphere@proton.me>
2025-07-30 13:42:25 +0530

    Fix test with app context

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,5 +3,6 @@ from app import app
 def test_home():
     with app.app_context():
         response = app.test_client().get('/')
+        assert response.status_code == 200
         assert b"Welcome to the, DevOps CI/CD World!" in response.data
 
