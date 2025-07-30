8c47ee1953e871ad49d6cf3ea14ce39585ba7dff
BobbyMarley001 <nexusphere@proton.me>
2025-07-30 12:44:57 +0530

    Add Bootstrap frontend and render HTML page

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-	return "Welcome to the, DevOps CI/CD World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-	app.run()
+    app.run(debug=True)
+
