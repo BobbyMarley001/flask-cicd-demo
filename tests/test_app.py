7c8e34496251074bdf21e4ee157857fb14f37da7
BobbyMarley001 <nexusphere@proton.me>
2025-07-30 13:39:53 +0530

    fix: use app context in test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
-import app
+from app import app
 
 def test_home():
-	assert app.home() == "Welcome to the, DevOps CI/CD World!"
+    with app.app_context():
+        response = app.test_client().get('/')
+        assert b"Welcome to the, DevOps CI/CD World!" in response.data
+
